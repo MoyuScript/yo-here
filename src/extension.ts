1dc9019f349c310d093cc1cee5de5397b29dbcca
MoyuScript <i@moyu.moe>
2021-12-13 20:22:05 +0800

    feat: Using terminal interface

--- src/extension.ts
+++ src/extension.ts
@@ -6,13 +6,10 @@ import * as yeoman from 'yeoman-environment';
 export function activate(context: vscode.ExtensionContext) {
 	
 	vscode.commands.registerCommand('yo-here.create', async (arg) => {
-		const env = yeoman.createEnv([], {}, );
-		const cwd = path.dirname(arg.path);
-		process.chdir(cwd);
+		const cwd = path.dirname(arg.fsPath);
 		
-		env.lookup({
-			packagePatterns: ['generator-snippet-*']
-		});
+		const env = yeoman.createEnv();
+		env.lookup();
 
 		const generators = env.getGeneratorNames();
 		
@@ -24,7 +21,18 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		env.run(pick, () => {});
+		let terminal = vscode.window.terminals.find((v) => v.name === 'yo');
+
+		if (terminal) {
+			terminal.dispose();
+		}
+		terminal = vscode.window.createTerminal({
+			name: 'yo',
+			cwd
+		});
+		
+		terminal.show(false);
+		terminal.sendText(`yo ${pick}`);
 	});
 }
 
